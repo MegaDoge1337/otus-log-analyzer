b36c1b5b9a34ee53b542b0e49572973971e8e86a
Aleksandr <gggols32@gmail.com>
2024-10-01 10:18:55 +0300

    Setup pre-commit hook (black)

--- src/app/module.py
+++ src/app/module.py
@@ -2,23 +2,21 @@ import time
 import json
 import sys
 
-config = {
-    "REPORT_SIZE": 1000,
-    "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
-}
+config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 def is_external_config_defined() -> bool:
-    return '--config' in sys.argv
+    return "--config" in sys.argv
 
 
 def get_external_config_path():
-    arg_key_index = sys.argv.index('--config')
+    arg_key_index = sys.argv.index("--config")
     try:
         return sys.argv[arg_key_index + 1]
     except IndexError:
-        print("usage python main.py --config [config_path]\noptions:\n--config  Defined path to external configuration file")
+        print(
+            "usage python main.py --config [config_path]\noptions:\n--config  Defined path to external configuration file"
+        )
         return None
 
 
@@ -36,11 +34,11 @@ def merge_configs(default_config: dict, external_config: dict):
     return default_config
 
 
-def main(config_path: str = None, log_path: str = None):
+def main():
     app_config = config
 
     if is_external_config_defined():
         external_config = read_external_config()
         app_config = merge_configs(app_config, external_config)
-    
+
     print(app_config)
